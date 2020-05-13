0731c8621bb59a31fb09f57ebf71f027098ce5f5
Rodrigo Anaice <rodrigoanaice@hotmail.com>
2020-05-13 13:27:14 -0300

    feat: add transaction item widget and related styles

--- lib/app/ui/pages/home_page.dart
+++ lib/app/ui/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:expensesmanager/app/model/transaction.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,14 +9,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _transactions = [
+    Transaction(id: 't1', title: 'Novo tênis de corrida', value: 310.76, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Conta de luz', value: 400, date: DateTime.now()),
+    Transaction(id: 't3', title: 'Playstation 3', value: 1200, date: DateTime.now()),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-      ),
-      body: Center(
-        child: Text('Primeira Versão'),
+        appBar: AppBar(
+          title: Text('Despesas Pessoais'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Card(
+              elevation: 5,
+              margin: EdgeInsets.all(8),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Gráfico'),
+              ),
+            ),
+            Column(children: [
+              ..._transactions.map((transaction) {
+                return _transactionItem(transaction);
+              }).toList()
+            ]),
+          ],
+        ));
+  }
+
+  Card _transactionItem(Transaction transaction) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.purple, width: 2),
+              ),
+              child: Text(
+                'R\$ ${transaction.value.toString()}',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.purple),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  transaction.title,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  transaction.date.toString(),
+                  style: TextStyle(fontSize: 12, color: Colors.black38),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
